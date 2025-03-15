ca598f05ca137c28b2b7341c95ddc16e4a95f7b9
Sanaia101 <skwashin@cougarnet.uh.edu>
2025-03-14 23:43:30 -0500

    Created footer and all contents inside. Deleted about page & created terms of service and privacy policy. Updated excursions tab and navigation bar.

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/callback', async (req, res) => {
         const userInfo = await client.userinfo(tokenSet.access_token);
         req.session.userInfo = userInfo;
 
-        res.redirect('/travelinquiryform');
+        res.redirect('/travel-inquiry-form');
     } catch (err) {
         console.error('Callback error:', err);
         res.redirect('/');
@@ -85,10 +85,7 @@ app.get('/logout', (req, res) => {
     res.redirect(logoutUrl);
 });
 
-app.get('/aboutus', function(req, res) {
-    res.render("about-us.ejs", {
-    });
-});
+
 
 app.get('/contact', function(req, res) {
     res.render("contact.ejs", {
@@ -110,10 +107,6 @@ app.get('/excursions', function(req, res) {
     });
 });
 
-app.get('/traveltermsandinsurance', function(req, res) {
-    res.render("Travel Terms & Insurance.ejs", {
-    });
-});
 
 function isAuthenticated(req, res, next) {
     if (req.session.userInfo) {
@@ -123,7 +116,7 @@ function isAuthenticated(req, res, next) {
     }
 }
 
-app.get('/travelinquiryform', isAuthenticated, async (req, res) => {
+app.get('/travel-inquiry-form', isAuthenticated, async (req, res) => {
     try {
         const userInfo = req.session.userInfo;
 
@@ -134,7 +127,7 @@ app.get('/travelinquiryform', isAuthenticated, async (req, res) => {
         });
 
         if (response.status === 200 || response.status === 201) {
-            res.render('Travel Inquiry Form.ejs', {
+            res.render('travel-inquiry-form.ejs', {
                 user_info: userInfo
             });
         } else {
@@ -144,6 +137,15 @@ app.get('/travelinquiryform', isAuthenticated, async (req, res) => {
         console.error(error);
         res.redirect('/');
     }
+});
+
+
+app.get('/terms-of-service', (req, res) => {
+    res.render('terms-of-service');
+});
+
+app.get('/privacy-policy', (req, res) => {
+    res.render('privacy-policy'); 
 });
 
 
